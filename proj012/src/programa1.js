7f3b45638b29bc293dc0711cc6927331d1bced27
Pedro Silva <pedro.nbk@gmail.com>
2019-08-18 00:30:17 -0300

    Projeto 012: Commit final

--- proj012/src/programa1.js
+++ proj012/src/programa1.js
@@ -9,7 +9,11 @@ fazer as chamadas, pois o programa de correção já vai fazer isso. Seu objetiv
 		Definir uma função chamada triplicarValor. Se essa função receber o número 3 
 		por exemplo, ela retorna 9. E se receber o número 100, ela retorna 300.	
 *******************************************************************************/
-
+function triplicarValor(num) {
+	num = prompt("digite um numero", 3);
+	num = num * 3;
+	return num;
+}
 
 
 
@@ -18,7 +22,10 @@ fazer as chamadas, pois o programa de correção já vai fazer isso. Seu objetiv
 		 Definir uma função chamada calcularComissao. Ela vai calcular a comissão dos 
 		 vendedores de uma loja. O percentual de comissão é 10%.
 *******************************************************************************/
-
+function calcularComissao(salario) {
+	var comissao = salario * 0.10;
+	return comissao;
+}
 
 
 
@@ -29,7 +36,10 @@ fazer as chamadas, pois o programa de correção já vai fazer isso. Seu objetiv
 		o primeiro referente a idade da pessoa (30 anos por exemplo) e o segundo 
 		referente a idade mínima para aposentar, 65 anos por exemplo.
 *******************************************************************************/
-
+function calculaAnosAposentadoria(idade, idadeMinima) {
+	var AnosRestantes = idadeMinima - idade;
+	return AnosRestantes;
+}
 
 
 
@@ -41,7 +51,11 @@ fazer as chamadas, pois o programa de correção já vai fazer isso. Seu objetiv
 		 Dentro dessa função, use a função criada na tarefa anterior para calcular 
 		 os anos que faltam para aposentadoria.
 *******************************************************************************/
-
+	function mostrarMensagem(idade, idadeMinima) {
+		let tempo = calculaAnosAposentadoria(idade, idadeMinima);
+		let resta = "Faltam "+tempo+" anos para a sua aposentadoria";
+		return resta;
+	}
 
 
 
@@ -51,7 +65,13 @@ fazer as chamadas, pois o programa de correção já vai fazer isso. Seu objetiv
 		temperatura está negativa. Se estiver abaixo de zero retorna true (verdadeiro) 
 		e se estiver acima de zero retorna false (falso)	
 *******************************************************************************/
-
+	function temperaturaNegativa(temperatura){
+		if (temperatura < 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 
 
 
@@ -71,7 +91,7 @@ https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects
 		https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/sqrt
 *******************************************************************************/
 
-let respostaTarefa6 = null;
+let respostaTarefa6 = Math.sqrt(289);
 
 
 
@@ -80,7 +100,8 @@ let respostaTarefa6 = null;
 		Quantos caracteres tem a frase "Ordem e progresso" ?
 *******************************************************************************/
 
-let respostaTarefa7 = null;
+var frase = "Ordem e progresso";
+let respostaTarefa7 = frase.length;
 
 
 
@@ -89,7 +110,7 @@ let respostaTarefa7 = null;
 		Qual é a sétima letra da palavra "Dinossauro"
 *******************************************************************************/
 
-let respostaTarefa8 = null;
+let respostaTarefa8 = "u";
 
 
 
@@ -98,8 +119,8 @@ let respostaTarefa8 = null;
 		Use uma função que leia a frase "Eu sei programar em Ruby" e substitua a 
 		palavra Ruby pela palavra Javascript.
 *******************************************************************************/
-
-let respostaTarefa9 = null;
+let texto = "Eu sei programar em Ruby";
+let respostaTarefa9 = texto.replace("Ruby", "Javascript");
 
 
 
@@ -108,4 +129,5 @@ let respostaTarefa9 = null;
 		Que dia do mês é hoje?
 *******************************************************************************/
 
-let respostaTarefa10 = null;
+let data = new Date();
+let respostaTarefa10 =  data.getDate();
