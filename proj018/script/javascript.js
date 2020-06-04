a624cf04c57f6a9ee268b880925142eb0966c90e
Pedro <pedro.nbk@gmail.com>
2020-06-04 18:59:57 -0300

    adição de metodos mostrarResposta e func do bota

--- proj018/script/javascript.js
+++ proj018/script/javascript.js
@@ -1,13 +1,36 @@
-function calculaIMC() {
+function pegarElemento(elemento) {
+    return document.querySelector(elemento);
+}
+
+function mostrarResultado() {
+    pegarElemento(".resultado").classList.remove("escondido");
+    pegarElemento(".botaoIMC").classList.add("escondido");
+}
+
+function mostrarBotao() {
+    pegarElemento(".resultado").classList.add("escondido");
+    pegarElemento(".botaoIMC").classList.remove("escondido");
+}
+
+pegarElemento(".botaoIMC").addEventListener("click",mostrarResultado, calculaIMC);
+pegarElemento(".voltar").addEventListener("click", mostrarBotao);
+
+
+ function calculaIMC() {
     var altura = document.getElementById("altura").value;
     var peso = document.getElementById("peso").value;
-    var botao = document.getElementsByClassName("botaoIMC");
 
     var resultado;
 
-    resultado = (peso / (altura * altura));
-
-    
-        botao.value = resultado + "muito pesado";
+    resultado = (parseInt(peso) / (parseInt(altura) * parseInt(altura)));
+    if (resultado < 16 ) {
+        const valorImc = pegarElemento(".imcValor");
+        valorImc.innerHTML = resultado;
+        const desIMC = pegarElemento(".imcDesc");
+        desIMC.innerHTML =  "Magreza Grave";
+    } else {
+        valorImc.innerHTML = 99;
+        desIMC.innerHTML = "gordao";
+    }
 }
 
