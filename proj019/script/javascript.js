fbf0fb1084da3df561d16c4df4293644c1412646
Pedro Silva <pedro.nbk@gmail.com>
2020-07-07 23:54:08 -0300

    Projeto 19 - atualizaçao codigo dos botoes

--- proj019/script/javascript.js
+++ proj019/script/javascript.js
@@ -3,10 +3,42 @@ function pegarElemento(elemento) {
 }
 
 pegarElemento("#botao1").addEventListener("click", function() {
-    adicionarNumero();
+    adicionarNumero(1);
 });
 
-function adicionarNumero() {
+pegarElemento("#botao2").addEventListener("click", function() {
+    adicionarNumero(2);
+});
+
+pegarElemento("#botao3").addEventListener("click", function() {
+    adicionarNumero(3);
+});
+
+pegarElemento("#botao4").addEventListener("click", function() {
+    adicionarNumero(4);
+});
+
+pegarElemento("#botao5").addEventListener("click", function() {
+    adicionarNumero(5);
+});
+
+pegarElemento("#botao6").addEventListener("click", function() {
+    adicionarNumero(6);
+});
+
+pegarElemento("#botao7").addEventListener("click", function() {
+    adicionarNumero(7);
+});
+
+pegarElemento("#botao8").addEventListener("click", function() {
+    adicionarNumero(8);
+});
+
+pegarElemento("#botao9").addEventListener("click", function() {
+    adicionarNumero(9);
+});
+
+function adicionarNumero(num) {
     var display = pegarElemento(".display");    
-    display.value += 1;
+    display.value += num;
 }
